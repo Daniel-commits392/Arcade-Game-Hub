cbb2a2781d9cdd3fd23108ffbec176b8484d8fc3
Shadow Nexus <coderdaniel6853@gmail.com>
2026-07-18 17:22:48 +0300

    Add dice roller game

--- gameHub.js
+++ gameHub.js
@@ -1,4 +1,6 @@
-const randomNumber = Math.floor(Math.random() * 100);
+ // GUESS THE NUMBER
+
+const randomNumber = Math.floor(Math.random() * 100 + 1);
 
 function guessNumber(number){
     const guess = number;
@@ -18,4 +20,29 @@ console.log(guessNumber(29))
 console.log(guessNumber(1))
 console.log(guessNumber(69))
 console.log(guessNumber(91))
-console.log(guessNumber(77))
+console.log(guessNumber(62))
+const year = 1513;
+
+
+// DICE ROLLER
+
+function diceRoller(){
+    const dice1 = Math.floor(Math.random() * 6 + 1); // produces the number 1,2,3,4,5,6
+    const dice2 = Math.floor(Math.random() * 6 + 1) ;
+
+    const total = dice1 + dice2
+    if(total <= 6){
+        return ` 🎲 dice 1: ${dice1} , 🎲 dice 2:${dice2} , Total: ${total}; Low Roll`
+    }
+    else if(total === 7){
+        return ` 🎲 dice 1: ${dice1} , 🎲 dice 2:${dice2} , Total: ${total}; Lucky Seven!`
+    }
+    else if(dice1 == dice2){
+        return `🎲 dice 1:${dice1}, 🎲 dice 2:${dice2}, Total: ${total}; 🎉 DOUBLE!`
+    }
+    else{
+        return `🎲 dice 1: ${dice1} 🎲 dice 2:${dice2}  Total: ${total} ;High Roll`;
+    }
+
+}
+console.log(diceRoller())
